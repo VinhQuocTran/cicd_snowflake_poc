f49805b43e3865781efcfd8ea5ac138f7928f226
Vinh Tran <vinhquoc2103@gmail.com>
2025-08-07 12:19:20 +0700

    as

--- changelogs/06_store_proc/001_create_insert_sp.sql
+++ changelogs/06_store_proc/001_create_insert_sp.sql
@@ -1,13 +1,18 @@
 -- liquibase formatted sql
 
 --changeset Vinh:1 logicalFilePath:sql/001_create_insert_sp.sql
-CREATE OR REPLACE PROCEDURE TEST_DB.PUBLIC.TMP_SP_TEST_5()
-RETURNS TABLE ("PACKAGE_NAME" VARCHAR, "VERSION" VARCHAR, "LANGUAGE" VARCHAR, "RUNTIME_VERSION" VARCHAR)
+CREATE OR REPLACE PROCEDURE OPERATION.SAC_MONITORING.SP_TEST_INSERT()
+RETURNS STRING
 LANGUAGE PYTHON
 RUNTIME_VERSION = '3.12'
-PACKAGES = ('snowflake-snowpark-python==1.33.0','cloudpickle==3.0.0')
-HANDLER = 'sp_test_1.main'
-IMPORTS = ('@MY_GIT_REPO/branches/main/sp_test_1.py')
+PACKAGES = ('snowflake-snowpark-python==1.33.0',
+        'cloudpickle==3.0.0',
+        'pandas==2.3.1',
+        'pyyaml==6.0.2'
+        )
+HANDLER = 'sp_test_insert.main'
+IMPORTS = ('@OPERATION.SAC_MONITORING.SAC_GIT_REPO/branches/master/python_stage/constant_and_util.py',
+        '@OPERATION.SAC_MONITORING.SAC_GIT_REPO/branches/master/store_proc/sp_test_insert.py')
 EXECUTE AS CALLER
 ;
 
